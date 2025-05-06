16e134f54c22ddb4591f1732054434de3de29e84
juanmelo20 <jcmelop@ufpso.edu.co>
2025-05-05 21:57:43 -0500

    base proyecto assura

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center h-screen">
+      <h1 className="text-3xl font-bold underline text-blue-800">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
